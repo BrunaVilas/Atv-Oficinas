92864e3260878e1038abe6e72b560d6fb5c87d37
Bruna Vilas Boas Ferreira <brunavilasboas.ferreira99@gmail.com>
2026-02-19 13:37:00 -0400

    Add files via upload

---
+++ manipular.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['oficinas'])) {
+    $_SESSION['oficinas'] = [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $nome = $_POST['nome'] ?? '';
+    $professor = $_POST['professor'] ?? '';
+    $horario = $_POST['horario'] ?? '';
+    $vagas = $_POST['vagas'] ?? '';
+    $status = $_POST['status'] ?? '';
+
+    if ($nome !== '' && $professor !== '' && $horario !== '' && $vagas !== '') {
+
+        $_SESSION['oficinas'][] = [
+            'nome' => $nome,
+            'professor' => $professor,
+            'horario' => $horario,
+            'vagas' => $vagas,
+            'status' => $status
+        ];
+
+        header("Location: index.php");
+        exit;
+    }
+}
+
+?>
